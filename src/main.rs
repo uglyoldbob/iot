561921da5490a1922a18e9df8e9492972afb9d90
Thomas Epperson <thomas.epperson@gmail.com>
2021-11-27 02:27:36 +0000

    Make the handler a bit more useful

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,50 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::server::conn::AddrStream;
+use std::collections::HashMap;
 
-async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World")))
+#[derive(Clone)]
+struct HttpContext {
+    dirmap : HashMap<String, fn() -> u32>
+}
+
+async fn handle(
+    context: HttpContext,
+    addr: SocketAddr,
+    req: Request<Body>
+    ) -> Result<Response<Body>, Infallible> {
+    let s = "Hello world";
+    let t = format!("{}{}",s, req.uri().path());
+    Ok(Response::new(Body::from(t)))
 }
 
 #[tokio::main]
 async fn main() {
+    let mut map : HashMap<String, fn() -> u32> = HashMap::new();
+    let hc = HttpContext {
+        dirmap: map.clone(),
+    };
+
     // Construct our SocketAddr to listen on...
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     // And a MakeService to handle each connection...
-    let make_service = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle))
+    let make_service = make_service_fn(move |conn: &AddrStream| {
+        let context = hc.clone();
+        let addr = conn.remote_addr();
+        async move {
+        Ok::<_, Infallible>(service_fn(move|req| {
+            let context = context.clone();
+            async move { 
+                    handle(context.clone(),addr,req).await}} ))
+        }
     });
 
     // Then bind and serve...
     let server = Server::bind(&addr).serve(make_service);
 
+    println!("Rust-iot server is running");
     // And run forever...
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
