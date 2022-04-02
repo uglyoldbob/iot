b30adeb790c7d77395072be7ae21338ff6fa3cf9
Thomas Epperson <thomas.epperson@gmail.com>
2022-04-02 12:06:12 -0400

    Add some placeholder functions and include the spongy castle stuff.

--- android/app/src/main/java/com/uglyoldbob/iotnode/MainActivity.java
+++ android/app/src/main/java/com/uglyoldbob/iotnode/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.security.Security;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
