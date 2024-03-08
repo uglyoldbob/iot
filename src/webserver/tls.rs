ee71d196eb18554369a24f690668783ef9fd01c1
Thomas Epperson <thomas.epperson@snapon.com>
2024-03-08 14:28:38 -0600

    Ready to decrypt private key of the pkcs12 container.

--- src/webserver/tls.rs
+++ src/webserver/tls.rs
@@ -4,9 +4,9 @@ use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::sync::Arc;
 
-use p12::yasna;
+use p12::yasna::{self, ASN1ErrorKind};
 
-use der::{Encode, Reader};
+use der::{Decode, Encode, Reader};
 use pkcs5::pbes2::Pbkdf2Params;
 use pkcs8::DecodePrivateKey;
 use tokio_rustls::rustls::crypto::CryptoProvider;
@@ -40,7 +40,9 @@ fn as_oid2(s: &'static str) -> const_oid::ObjectIdentifier {
 }
 
 lazy_static::lazy_static! {
-    static ref OID_DATA_CONTENT_TYPE: yasna::models::ObjectIdentifier = as_oid(&[1, 2, 840, 113_549, 1, 7, 1]);
+    static ref OID_PKCS7_DATA_CONTENT_TYPE: yasna::models::ObjectIdentifier = as_oid(&[1, 2, 840, 113_549, 1, 7, 1]);
+    static ref OID_SHROUDED_KEY_BAG: yasna::models::ObjectIdentifier = as_oid(&[1, 2, 840, 113_549, 1, 12, 10, 1, 2]);
+    static ref OID_CERT_BAG: yasna::models::ObjectIdentifier = as_oid(&[1, 2, 840, 113_549, 1, 12, 10, 1, 3]);
     static ref OID_PKCS7_ENCRYPTED_DATA_CONTENT_TYPE: yasna::models::ObjectIdentifier =
         as_oid(&[1, 2, 840, 113_549, 1, 7, 6]);
     static ref OID_PKCS5_PBES2: yasna::models::ObjectIdentifier =
@@ -222,6 +224,134 @@ impl Pkcs5Pbes2 {
     }
 }
 
+#[derive(Debug)]
+struct PkiMessage {
+    cert: x509_cert::Certificate,
+}
+
+impl PkiMessage {
+    fn parse(data: &[u8]) -> Result<Self, ASN1Error> {
+        let d = yasna::parse_der(data, |r| {
+            r.read_sequence(|r| {
+                let d = r
+                    .next()
+                    .read_sequence(|r| {
+                        let oid = r.next().read_oid()?;
+                        let cert = if oid == *OID_CERT_BAG {
+                            let certdata = r
+                                .next()
+                                .read_tagged(yasna::Tag::context(0), |r| {
+                                    let bag = p12::CertBag::parse(r)?;
+                                    if let p12::CertBag::X509(x509) = bag {
+                                        let mut reader = der::SliceReader::new(&x509).unwrap();
+                                        let cert = x509_cert::certificate::Certificate::decode(
+                                            &mut reader,
+                                        )
+                                        .expect("Failed to read x509");
+                                        return Ok(cert);
+                                    }
+                                    Err(ASN1Error::new(yasna::ASN1ErrorKind::Invalid))
+                                })
+                                .expect("Failed parse 17");
+                            certdata
+                        } else {
+                            panic!("Unexpected oid for certificate");
+                        };
+                        r.next().read_set_of(|r| {
+                            r.read_sequence(|r| {
+                                let oid = r.next().read_oid()?;
+                                if oid == *OID_PKCS9_LOCAL_KEY_ID {
+                                    r.next().read_set_of(|r| {
+                                        let d = r.read_bytes()?;
+                                        Ok(())
+                                    })?;
+                                } else if oid == *OID_PKCS9_FRIENDLY_NAME {
+                                    r.next().read_set_of(|r| {
+                                        let name = r.read_bmp_string()?;
+                                        Ok(())
+                                    })?;
+                                }
+                                Ok(42)
+                            })?;
+                            Ok(())
+                        })?;
+                        Ok(cert)
+                    })
+                    .expect("Failed parse 18");
+                Ok(d)
+            })
+        })?;
+        Ok(Self { cert: d })
+    }
+}
+
+#[derive(Debug)]
+struct X509Request {
+    key: p12::EncryptedPrivateKeyInfo,
+}
+
+impl X509Request {
+    fn decrypt(&self, pass: &[u8]) -> Option<Vec<u8>> {
+        if let p12::AlgorithmIdentifier::OtherAlg(o) = &self.key.encryption_algorithm {
+            println!("Algorithm type is {:?}", o.algorithm_type);
+        }
+        None
+    }
+
+    fn parse(data: &[u8], pass: &[u8]) -> Result<Self, ASN1Error> {
+        yasna::parse_der(data, |r| {
+            r.read_sequence(|r| {
+                r.next().read_sequence(|r| {
+                    let oid = r.next().read_oid()?;
+                    let pkey = if oid == *OID_SHROUDED_KEY_BAG {
+                        let pkey = r.next().read_tagged(yasna::Tag::context(0), |r| {
+                            p12::EncryptedPrivateKeyInfo::parse(r)
+                        })?;
+                        Ok(Self { key: pkey })
+                    } else {
+                        Err(ASN1Error::new(yasna::ASN1ErrorKind::Invalid))
+                    };
+                    let pkey = pkey?;
+                    let s = r.next().read_set_of(|r| {
+                        let s = r.read_sequence(|r| {
+                            let oid = r.next().read_oid().expect("Failed to read oid");
+                            println!("Oid5 is {:?}", oid);
+                            if oid == *OID_PKCS9_LOCAL_KEY_ID {
+                                r.next()
+                                    .read_set(|r| {
+                                        let d = r
+                                            .next(&[yasna::tags::TAG_OCTETSTRING])?
+                                            .read_bytes()
+                                            .expect("Failed to read local key id");
+                                        Ok(())
+                                    })
+                                    .expect("Failed to read sequence 2");
+                            } else if oid == *OID_PKCS9_FRIENDLY_NAME {
+                                r.next()
+                                    .read_set(|r| {
+                                        let d = r
+                                            .next(&[yasna::tags::TAG_BMPSTRING])?
+                                            .read_bmp_string()
+                                            .expect("Failed to read friendly name");
+                                        Ok(())
+                                    })
+                                    .expect("Failed to read sequence 3");
+                            } else {
+                                panic!("Unexpected oid in public key info");
+                            }
+                            Ok(())
+                        });
+                        println!("Sequence return is {:?}", s);
+                        s
+                    });
+                    s?;
+                    Ok(pkey)
+                })
+            })
+        })
+    }
+}
+
 fn safe_bags(ec: &p12::PFX, pass: &[u8]) -> Result<Vec<p12::SafeBag>, ASN1Error> {
     let data = ec.auth_safe.data(pass).unwrap();
     let safe_bags = yasna::parse_ber(&data, |r| r.collect_sequence_of(p12::SafeBag::parse))?;
@@ -250,28 +380,52 @@ where
     let thing2a = ec.auth_safe.oid();
     let thing2 = safe_bags(&ec, pass.as_bytes()).expect("Problem reading bags");
     println!("PFX bags {}:", thing2.len());
+
+    let mut certificate = None;
+    let mut pkey = None;
+
     for b in thing2.iter() {
-        use der::Decode;
-        let data = b.bag.other_bag_data().expect("Expected other bag data");
-        if let Ok(bag) = cms::encrypted_data::EncryptedData::from_der(&data) {
-            let algorithm = bag.enc_content_info.content_enc_alg;
-            let bag_data = bag.enc_content_info.encrypted_content.unwrap();
-            println!("Algorithm is {:?}", algorithm.oid);
-            if algorithm.oid == pkcs5::pbes2::PBES2_OID {
-                let a =
-                    Pkcs5Pbes2::parse_parameters(algorithm.parameters.unwrap().value()).unwrap();
-                let bdv = bag_data.into_bytes();
-                let decrypted = a.decrypt(&bdv, pass.as_bytes());
-                let decrypted_oid = bag.enc_content_info.content_type;
-                println!("Decrypted der data is {:?} {:X?}", decrypted_oid, decrypted);
-                for a in &decrypted {
-                    print!("{:02X}", a);
+        let mob = if let p12::SafeBagKind::OtherBagKind(o) = &b.bag {
+            Some(o)
+        } else {
+            None
+        };
+        let ob = mob.expect("Expected other bag data");
+        if ob.bag_id == *OID_PKCS7_ENCRYPTED_DATA_CONTENT_TYPE {
+            if let Ok(bag) = cms::encrypted_data::EncryptedData::from_der(&ob.bag_value) {
+                let algorithm = bag.enc_content_info.content_enc_alg;
+                let bag_data = bag.enc_content_info.encrypted_content.unwrap();
+                if algorithm.oid == pkcs5::pbes2::PBES2_OID {
+                    let a = Pkcs5Pbes2::parse_parameters(algorithm.parameters.unwrap().value())
+                        .unwrap();
+                    let bdv = bag_data.into_bytes();
+                    let decrypted = a.decrypt(&bdv, pass.as_bytes());
+                    let decrypted_oid = bag.enc_content_info.content_type;
+                    if decrypted_oid == *OID2_DATA_CONTENT_TYPE {
+                        let cert = PkiMessage::parse(&decrypted)
+                            .expect("Failed to parse certificate data");
+                        certificate = Some(cert);
+                    }
+                } else {
+                    panic!("Unexpected algorithm {:?}", algorithm.oid);
                 }
-                println!("");
             }
+        } else if ob.bag_id == *OID_PKCS7_DATA_CONTENT_TYPE {
+            let bag_data = yasna::parse_der(&ob.bag_value, |r| r.read_bytes())
+                .expect("Failed to read bag data");
+            let req =
+                X509Request::parse(&bag_data, pass.as_bytes()).expect("Failed to read request");
+            let p = req
+                .decrypt(pass.as_bytes())
+                .expect("Failed to decrypt private key");
+            println!("The request is {:?}", p);
+            pkey = Some(p);
+        } else {
+            todo!("Handle bag {:X?}", ob.bag_id);
         }
-        todo!("Parse with cms");
     }
+    println!("Certificate is {:?}", certificate);
+    todo!("Convert to what is needed");
     let thing3 = ec.mac_data;
     if let Some(thing3) = thing3 {
         println!("PFX MAC data is {:?}", thing3);
