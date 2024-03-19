33b4d2ca62e2fe248deda5f8da4c32a37f90e520
Thomas Epperson <thomas.epperson@snapon.com>
2024-03-19 09:48:48 -0500

    Create a settings example file. Copy example settings to real settings if real settings do not exist. Only run npm if it is runnable.

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=js");
 
     // false - run npm out of the source directory
     // true - run npm out of the build directory
@@ -10,6 +9,12 @@ fn main() {
         std::path::PathBuf::from(std::env::var("OUT_DIR").expect("No output directory given"));
     let source_path =
         std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("No source directory"));
+
+    let settings = source_path.join("settings.ini");
+    if !settings.exists() {
+        let example = source_path.join("settings.example.ini");
+        std::fs::copy(example, settings).unwrap();
+    }
 
     let p = if use_out {
         let p = out_path.join("js");
@@ -25,36 +30,45 @@ fn main() {
         source_path.join("js")
     };
 
-    let npm_output = std::process::Command::new("npm")
+    let npm_test = std::process::Command::new("npm")
         .current_dir(&p)
-        .arg("install")
-        .status()
-        .expect("Failed to run npm");
-    if !npm_output.success() {
-        panic!("Npm install failed");
-    }
+        .arg("--version")
+        .status();
 
-    let nfp = p.join("node_modules");
-    let filenames = [
-        ("node-forge/dist/forge.all.min.js", "forge.all.min.js"),
-        ("node-forge/dist/forge.min.js", "forge.min.js"),
-        ("node-forge/dist/forge.min.js.map", "forge.min.js.map"),
-        ("node-forge/dist/prime.worker.min.js", "prime.worker.min.js"),
-        ("certgen/index.min.js", "certgen.min.js"),
-    ];
-    if use_out {
-        for (f, g) in filenames {
-            let src = nfp.join(f);
-            std::fs::copy(src, out_path.join(g)).expect(&format!("Failed to copy js file {}", f));
+    if npm_test.is_ok() {
+        println!("cargo:rerun-if-changed=js");
+        let npm_output = std::process::Command::new("npm")
+            .current_dir(&p)
+            .arg("install")
+            .status()
+            .expect("Failed to run npm");
+        if !npm_output.success() {
+            panic!("Npm install failed");
         }
-    } else {
-        let p = source_path.join("content").join("js");
-        std::fs::create_dir_all(&p).expect("Failed to create file for npm built files");
-        for (f, g) in filenames {
-            let src = nfp.join(f);
-            let dst = p.join(g);
-            std::fs::copy(&src, dst)
-                .expect(&format!("Failed to copy js file {}", src.to_str().unwrap()));
+
+        let nfp = p.join("node_modules");
+        let filenames = [
+            ("node-forge/dist/forge.all.min.js", "forge.all.min.js"),
+            ("node-forge/dist/forge.min.js", "forge.min.js"),
+            ("node-forge/dist/forge.min.js.map", "forge.min.js.map"),
+            ("node-forge/dist/prime.worker.min.js", "prime.worker.min.js"),
+            ("certgen/index.min.js", "certgen.min.js"),
+        ];
+        if use_out {
+            for (f, g) in filenames {
+                let src = nfp.join(f);
+                std::fs::copy(src, out_path.join(g))
+                    .expect(&format!("Failed to copy js file {}", f));
+            }
+        } else {
+            let p = source_path.join("content").join("js");
+            std::fs::create_dir_all(&p).expect("Failed to create file for npm built files");
+            for (f, g) in filenames {
+                let src = nfp.join(f);
+                let dst = p.join(g);
+                std::fs::copy(&src, dst)
+                    .expect(&format!("Failed to copy js file {}", src.to_str().unwrap()));
+            }
         }
     }
 }
