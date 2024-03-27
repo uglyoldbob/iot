8d4ba22fa220b532497c9e011ad36d78fe3776bb
Thomas Epperson <thomas.epperson@snapon.com>
2024-03-27 13:15:07 -0500

    Clean up the test program a bit.

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,5 @@
+use prompt::Prompting;
+
 #[derive(Debug, prompt::Prompting)]
 struct TestMe {
     bob: u8,
@@ -16,7 +18,7 @@ async fn main() {
     println!("Running test program");
 
     println!("Please enter a value");
-    let s = <TestMe as prompt::Prompting>::prompt(None);
+    let s = TestMe::prompt(None);
     if let Ok(s) = s {
         println!("You entered {:?}", s);
     }
