7a05dde6c70f1f6bb0ff49ca6e1cdce4a9e59b18
Thomas Epperson <thomas.epperson@snapon.com>
2024-05-01 15:52:48 -0500

    Skeleton wasm project.

--- src/test2.rs
+++ src/test2.rs
@@ -21,13 +21,54 @@ pub async fn verify_request2(
     })
     .unwrap();
 
+    assert!(untrusted::Input::from(&pkey)
+                .read_all(ring::error::Unspecified, |input| ring::io::der::nested(
+                    input,
+                    ring::io::der::Tag::Sequence,
+                    ring::error::Unspecified,
+                    |input| {
+                        let _ = ring::io::der::positive_integer(input)?;
+                        let _ = ring::io::der::positive_integer(input)?;
+                        Ok(())
+                    }
+                ))
+                .is_ok());
+
+    println!("Public key is {:02X?}", pkey);
+
     let csr_key = UnparsedPublicKey::new(&RSA_PKCS1_2048_8192_SHA256, &pkey);
     println!("Cert is {:?}", csr_key);
     println!("info {:02X?}", info);
-    csr_key
+    println!("Signature is {:02X?}", signature.as_bytes().unwrap());
+
+    let info2 = [info.clone(), vec![0x80 as u8]].concat();
+
+    let manual_sha256 = sha256::digest(&info);
+    println!("Manual sha256 is {}", manual_sha256);
+
+    use sha2::Digest;
+    use sha2::digest::core_api::BlockSizeUser;
+    let mut hash2 = sha2::Sha256::new();
+    println!("STUFF 1 {}", sha2::Sha256::block_size());
+    hash2.update(&info);
+    let manual_sha2 = hash2.finalize();
+    println!("Manual sha2 is {:02X?}", manual_sha2);
+
+    let info2 = [&info, signature.as_bytes().unwrap()].concat();
+
+    let e1 = csr_key
         .verify(&info, signature.as_bytes().unwrap())
         .map_err(|_| {
             println!("Error verifying the signature on the csr 1");
+        });
+    if e1.is_ok() {
+        println!("SUCCESS VALIDATE SIGNATURE");
+    }
+
+    csr_key
+        .verify(&info2, signature.as_bytes().unwrap())
+        .map_err(|_| {
+            println!("Error verifying the signature2 on the csr 1");
         })?;
     //TODO perform more validation of the csr
     Ok(csr)
@@ -38,7 +79,7 @@ async fn main() {
     println!("Running test2 program");
 
     let mut settings_con = Vec::new();
-    let mut f = tokio::fs::File::open("./cert.pub").await.unwrap();
+    let mut f = tokio::fs::File::open("./server.csr").await.unwrap();
     f.read_to_end(&mut settings_con).await.unwrap();
     let pem = std::str::from_utf8(&settings_con).unwrap();
     println!("{}", pem);
