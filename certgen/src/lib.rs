00b4aa3c89a03dedd115cdb89578452591e9a0e8
Thomas Epperson <thomas.epperson@snapon.com>
2024-05-07 14:00:33 -0500

    Start parsing the form for creating a csr on the request page.

--- certgen/src/lib.rs
+++ certgen/src/lib.rs
@@ -7,15 +7,28 @@ extern "C" {
     fn alert(s: &str);
 }
 
-fn get_value_from_input_by_name(d: &web_sys::Document, name: &str) -> Option<String> {
+fn get_html_input_by_name(d: &web_sys::Document, name: &str) -> Option<web_sys::HtmlInputElement> {
     if let Some(t1) = d.get_element_by_id(name) {
         let jsval : wasm_bindgen::JsValue = t1.value_of().into();
-        if let Ok(hie) = web_sys::HtmlInputElement::try_from(jsval) {
-            Some(hie.value())
-        }
-        else {
-            None
-        }
+        web_sys::HtmlInputElement::try_from(jsval).ok()
+    }
+    else {
+        None
+    }
+}
+
+fn get_value_from_input_by_name(d: &web_sys::Document, name: &str) -> Option<String> {
+    if let Some(t1) = get_html_input_by_name(d, name) {
+        Some(t1.value())
+    }
+    else {
+        None
+    }
+}
+
+fn get_checked_from_input_by_name(d: &web_sys::Document, name: &str) -> Option<bool> {
+    if let Some(t1) = get_html_input_by_name(d, name) {
+        Some(t1.checked())
     }
     else {
         None
@@ -24,39 +37,82 @@ fn get_value_from_input_by_name(d: &web_sys::Document, name: &str) -> Option<Str
 
 #[wasm_bindgen]
 pub fn generate_csr_rsa_sha256() {
+    crate::utils::set_panic_hook();
     let mut params: rcgen::CertificateParams = Default::default();
 
     let w = web_sys::window().unwrap();
     let d = w.document().unwrap();
-    if let Some(t1) = get_value_from_input_by_name(&d, "name") {
-        alert(&t1);
+    let name = get_value_from_input_by_name(&d, "name");
+    let email = get_value_from_input_by_name(&d, "email");
+    let phone = get_value_from_input_by_name(&d, "phone");
+    let private_key_password = get_value_from_input_by_name(&d, "password");
+
+    let client_id = get_checked_from_input_by_name(&d, "usage-client");
+    let code_usage = get_checked_from_input_by_name(&d, "usage-code");
+    let server_id = get_checked_from_input_by_name(&d, "usage-server");
+
+    let cname = get_value_from_input_by_name(&d, "cname");
+    let country = get_value_from_input_by_name(&d, "country");
+    let state = get_value_from_input_by_name(&d, "state");
+    let locality = get_value_from_input_by_name(&d, "locality");
+    let organization = get_value_from_input_by_name(&d, "organization");
+    let ou = get_value_from_input_by_name(&d, "organization-unit");
+    let cpassword = get_value_from_input_by_name(&d, "challenge-pass");
+    let challenge_name = get_value_from_input_by_name(&d, "challenge-name");
+
+    let mut good_name = false;
+
+    params.distinguished_name = rcgen::DistinguishedName::new();
+    if let Some(cname) = cname {
+        if !cname.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::CommonName, cname);
+        }
     }
-    else {
-        alert("No name");
+    if let Some(country) = country {
+        if !country.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::CountryName, country);
+        }
     }
-    if let Some(t1) = get_value_from_input_by_name(&d, "email") {
-        alert(&t1);
+    if let Some(state) = state {
+        if !state.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::StateOrProvinceName, state);
+        }
     }
-    else {
-        alert("No email");
+    if let Some(locality) = locality {
+        if !locality.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::LocalityName, locality);
+        }
+    }
+    if let Some(organization) = organization {
+        if !organization.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::OrganizationName, organization);
+        }
+    }
+    if let Some(ou) = ou {
+        if !ou.is_empty() {
+            good_name = true;
+            params.distinguished_name.push(rcgen::DnType::OrganizationalUnitName, ou);
+        }
     }
 
+    // These values are ignored
     params.not_before = rcgen::date_time_ymd(1975, 1, 1);
     params.not_after = rcgen::date_time_ymd(4096, 1, 1);
-    params.distinguished_name = rcgen::DistinguishedName::new();
-    params
-        .distinguished_name
-        .push(rcgen::DnType::OrganizationName, "Crab widgits SE");
-    params
-        .distinguished_name
-        .push(rcgen::DnType::CommonName, "Master Cert");
+
+    //TODO implement these items
+    /*
     params.subject_alt_names = vec![
         rcgen::SanType::DnsName("crabs.crabs".try_into().unwrap()),
         rcgen::SanType::DnsName("localhost".try_into().unwrap()),
     ];
-
-    let key_pair = rcgen::KeyPair::generate();
-    if let Ok(key_pair) = key_pair {
+    */
+    let signing = cert_common::CertificateSigningMethod::RsaSha256;
+    if let Some((key_pair, private)) = signing.generate_keypair() {
         if let Ok(cert) = params.self_signed(&key_pair) {
             let pem_serialized = cert.pem();
             if let Ok(pem) = pem::parse(&pem_serialized) {
